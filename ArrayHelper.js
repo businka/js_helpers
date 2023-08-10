06a5501d1ff9ec6f6433957008870586e1ab08a6
businka <1338833@gmail.com>
2023-08-10 18:40:35 +0300

    no message

--- ArrayHelper.js
+++ ArrayHelper.js
@@ -1,30 +1,22 @@
-module.exports = {
-  findIndexInArrayObj,
-  findInArrayObj,
-  sortArrayNumber,
-  sortArrayObject
-}
-
-
-function findInArrayObj (_array, finding_value, field='id') {
+export function findInArrayObj (_array, finding_value, field='id') {
   if (!field) throw new Error('findIndexInArrayObj: field for find not defined')
   return _array.find(function(item){
     return item[field] === finding_value
   })
 }
 
-function findIndexInArrayObj (_array, finding_value, field='id') {
+export function findIndexInArrayObj (_array, finding_value, field='id') {
   if (!field) throw new Error('findIndexInArrayObj: field for find not defined')
   return _array.findIndex(function(item){
     return item[field] === finding_value
   })
 }
 
-function sortArrayNumber(arr) {
+export function sortArrayNumber(arr) {
   return arr.sort((a, b) => a - b)
 }
 
-function sortArrayObject(arr, key) {
+export function sortArrayObject(arr, key) {
   arr.sort(function (a, b) {
     let keyA = a[key].toLowerCase()
     let keyB = b[key].toLowerCase()
